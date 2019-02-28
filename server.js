f4f95b81ad35f3679e1dc1caa29b68648238d707
edo9k <edox86@gmail.com>
2019-02-28 11:26:06 -0300

    Testing upload with storage setting
    Also tested saving the file with original name and with another name
    generated by the date function. Created some random explanation page
    that somehow gets rewritten by the upload page.

--- server.js
+++ server.js
@@ -1,12 +1,62 @@
 const express = require('express'),
       app = express(),
-      multer = require('multer')
+      multer = require('multer'),
+      path = require('path'),
+      fs = require('fs')
 
-const upload = multer({ dest: 'upload/' })
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'upload/')
+  },
+  filename: function (req, file, cb) {
+
+    // salvando com a mesma extensão
+    cb(null, `${file.fieldname}-${Date.now()}.${path.extname(file.originalname)}`)
+
+    //salvando com o mesmo nome
+    //cb(null, file.originalname)
+    
+  }
+})
+
+
+//const upload = multer({ dest: 'upload/' })
+const upload = multer({ storage });
+
+const trashFiles = (req, res, next) => {
+  fs.readdir('upload/', (err, files) => {
+    if (err) throw err
+
+    for (const file of files) {
+      fs.unlink(path.join('upload/', file), err => {
+        if (err) throw err;
+      })
+    }
+  })
+
+  next()
+}
 
 app.use(express.static('public'))
 
 app.post('/file/upload', upload.single('file'),
   (req, res) => res.send('<h2>Upload uploaded com sucesso.</h2>'))
+
+app.get('/trash', trashFiles,
+  (req, res) => res.send(`<h2>Arquivos deveriam ter sido apagados. (@${Date.now()})</h2>`))
+
+app.get('/file/list', 
+  (req, res) => res.send('meh'))
+
+app.get('/wot', (req, res) => res.send(`
+      <h2>Upload simples com JS+Node+Express+Multer</h2>
+      <ul>
+        <li> <a href="/file/upload"> Faz um upload de arquivo. </a> </li>
+        <li> <a href="/file/list"> Lista arquivos em ./upload/ </a> </li>
+        <li> <a href="/trash"> Apaga os arquivos da pasta ./upload/ </a> </li>
+      </ul>
+    `))
+
 
 app.listen(3000, () => console.log('Rodando em localhost:3k'))
