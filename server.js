9abe2ecea85a908da1150e33d0afcdde6221ad40
edo9k <edox86@gmail.com>
2019-03-07 14:42:52 -0300

    Set upload with original name
    The next step is to sanitize the name before saving.

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express'),
+      bodyParser = require('body-parser'),
       app = express(),
       multer = require('multer'),
       path = require('path'),
@@ -7,22 +8,26 @@ const express = require('express'),
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'upload/')
+    cb(null, './upload/')
   },
   filename: function (req, file, cb) {
 
     // salvando com a mesma extensão
-    cb(null, `${file.fieldname}-${Date.now()}.${path.extname(file.originalname)}`)
+    /* console.log(`
+    ---
+    ${file}
+    ---
+    ${req}
+    ---
+    `); */
 
-    //salvando com o mesmo nome
-    //cb(null, file.originalname)
-    
+    cb(null, `${Date.now()}-${file.originalname}`)
   }
 })
 
 
 //const upload = multer({ dest: 'upload/' })
-const upload = multer({ storage });
+const upload = multer({ storage })
 
 const trashFiles = (req, res, next) => {
   fs.readdir('upload/', (err, files) => {
@@ -41,22 +46,13 @@ const trashFiles = (req, res, next) => {
 app.use(express.static('public'))
 
 app.post('/file/upload', upload.single('file'),
-  (req, res) => res.send('<h2>Upload uploaded com sucesso.</h2>'))
+  (req, res) => res.send('<h2>Arquivo enviado com sucesso.</h2>'))
 
 app.get('/trash', trashFiles,
-  (req, res) => res.send(`<h2>Arquivos deveriam ter sido apagados. (@${Date.now()})</h2>`))
+  (req, res) => res.send(`<h2>Arquivos apagados.</h2> <sub>@${ new Date() }</sub>`))
+
 
 app.get('/file/list', 
   (req, res) => res.send('meh'))
-
-app.get('/wot', (req, res) => res.send(`
-      <h2>Upload simples com JS+Node+Express+Multer</h2>
-      <ul>
-        <li> <a href="/file/upload"> Faz um upload de arquivo. </a> </li>
-        <li> <a href="/file/list"> Lista arquivos em ./upload/ </a> </li>
-        <li> <a href="/trash"> Apaga os arquivos da pasta ./upload/ </a> </li>
-      </ul>
-    `))
-
 
 app.listen(3000, () => console.log('Rodando em localhost:3k'))
